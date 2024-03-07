46831268e102e42daa83b3f00e94277f51706c44
Kristo Veber <kristoveber7@gmail.com>
2024-03-07 18:37:45 +0200

    Add flag for HTTP network address and start server on specified address

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"social-network/internal/routes"
+)
+
+
 func main() {
-	// This is a comment
-	println("Hello, World!")
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	srv := &http.Server {
+		Addr: *addr,
+		Handler: routes.Routes(),
+	}
+
+	infoLog.Printf("Starting server on %s\n", *addr)
+	log.Fatal(srv.ListenAndServe())
 }
