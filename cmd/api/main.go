afebb26ba96b2a3f3d7138a986664f9f3f31d207
Kristo Veber <kristoveber7@gmail.com>
2024-03-19 12:29:31 +0200

    refactor code and add JSON reading and writing helpers

---
+++ cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	logger *log.Logger
+}
+
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	
+	flag.Parse()
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	
+	app := &application{
+		logger: logger,
+	}
+	
+	srv := &http.Server{
+		Addr:    *addr,
+		Handler: app.routes(),
+	}
+
+	logger.Printf("Starting server on %s\n", *addr)
+	log.Fatal(srv.ListenAndServe())
+}
