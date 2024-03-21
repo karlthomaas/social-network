26f5751c70631c71d336965731df6fc370102cb7
Karl-Thomas <karl-thomas@zink.ee>
2024-03-21 17:21:50 +0200

    Add fetchers module for making API requests

---
+++ frontend/src/lib/fetchers.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+   // withCredentials: true,
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-CSRFToken',
+    headers: { 'Content-Type': 'application/json' },
+});
+
+export default axiosInstance;
+
+
+export const fetcherWithOptions = ({ url, method, body }: { url: string, method:string, body: any}) => {
+
+    // const env = process.env.NODE_ENV;
+    // const token = Cookies.get('csrftoken');
+    // const config =
+    //     env === 'development'
+    //         ? {
+    //               headers: {
+    //                   'X-CSRFToken': token,
+    //               },
+    //           }
+
+    //         : {};
+
+    const config = {};
+    switch (method) {
+        case 'OPTIONS': {
+            return axiosInstance.options(url).then((res) => res.data);
+        }
+        case 'DELETE': {
+            return axiosInstance.delete(url, { ...config }).then((res) => res.data);
+        }
+        case 'PUT': {
+            return axiosInstance.put(url, JSON.stringify(body), { ...config }).then((res) => res.data);
+        }
+        case 'PATCH': {
+            return axiosInstance.patch(url, JSON.stringify(body), { ...config }).then((res) => res.data);
+        }
+        case 'POST': {
+            return axiosInstance.post(url, JSON.stringify(body), { ...config }).then((res) => res.data);
+        }
+        default:
+            return axiosInstance.get(url).then((res) => res.data);
+    }
+};
+
+
+export const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
