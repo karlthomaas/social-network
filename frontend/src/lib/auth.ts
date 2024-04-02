8c03fb77aa7bc0fd4baa113e11ddb3bebe251b2f
Karl-Thomas <karl-thomas@zink.ee>
2024-04-02 19:40:58 +0300

    Update backend URL in auth.ts

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -11,7 +11,7 @@ const getJwtSecretKey = () => {
 
 export const refreshSession = async (token: string, refreshToken: string): Promise<string | null> => {
   try {
-    const res = await fetch('http://backend:4000/api/authenticate', {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/authenticate`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`,
