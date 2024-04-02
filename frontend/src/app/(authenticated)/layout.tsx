7f3eaa0ddd8ad4c33c0b44f77b6b6c3531f4f824
Karl-Thomas <karl-thomas@zink.ee>
2024-04-02 22:05:02 +0300

    Refactor layout component to remove unused code

--- frontend/src/app/(authenticated)/layout.tsx
+++ frontend/src/app/(authenticated)/layout.tsx
@@ -1,25 +1,5 @@
 import React from 'react';
-import axios from 'axios';
-import { fetcher, fetcherWithOptions } from '@/lib/fetchers';
-import { cookies } from 'next/headers';
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  // get current cookies
-  console.log(cookies());
-  const jwtCookie = cookies().get('Token');
-  const refreshToken = cookies().get('Refresh-Token');
-
-  try {
-    const res = await fetcherWithOptions({
-      url: 'http://backend:4000/api/authenticate',
-      method: 'GET',
-      body: null, // Add this line
-      headers: { Authorization: `Bearer ${jwtCookie?.value}`, Cookie: `Refresh-Token=${refreshToken?.value};`},
-    });
-    console.log(res);
-  } catch (error) {
-    console.error(error.response.data);
-  }
-
+export default async function Layout({ children, params }: { children: React.ReactNode, params: any }) {
   return <div>{children}</div>;
 }
