6157e9bca6046ae68c2cc02db50f8bde3d8e193c
Karl-Thomas <karl-thomas@zink.ee>
2024-04-02 19:40:49 +0300

    Add auth.ts file for authentication and session management

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { jwtVerify } from 'jose';
+
+const getJwtSecretKey = () => {
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret || secret.length === 0) {
+    throw new Error('JWT_SECRET not defined');
+  }
+  return secret;
+};
+
+export const refreshSession = async (token: string, refreshToken: string): Promise<string | null> => {
+  try {
+    const res = await fetch('http://backend:4000/api/authenticate', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Cookie: `Refresh-Token=${refreshToken};`,
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.headers.get('set-cookie');
+  } catch (error) {
+    return null;
+  }
+};
+
+export const verifyAuth = async (token: string) => {
+  const verified = await jwtVerify(token, new TextEncoder().encode(getJwtSecretKey()));
+  return verified.payload;
+};
