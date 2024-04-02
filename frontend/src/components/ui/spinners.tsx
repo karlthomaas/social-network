42214c5946b61a4466ecafbaa299a5b03c9e5a18
Karl-Thomas <karl-thomas@zink.ee>
2024-04-02 19:41:04 +0300

    Add LoadingSpinner component

---
+++ frontend/src/components/ui/spinners.tsx
@@ -0,0 +1,11 @@
+import { Oval } from "react-loader-spinner";
+
+export const LoadingSpinner = () => {
+    return (
+        <Oval
+        color="#000"
+        height={40}
+        width={40}
+        />
+    );
+};
