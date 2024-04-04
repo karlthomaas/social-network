eafb087c12ab8dd06814a2510ae9bbd8a854386e
Kristo Veber <kristoveber7@gmail.com>
2024-04-04 14:28:05 +0300

    Refactor routes in routes.go

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,13 +7,22 @@ import (
 func (app *application) routes() http.Handler {
 	router := http.NewServeMux()
 
-	router.Handle("GET /", app.ValidateJwt(app.HomeHandler))
-	router.HandleFunc("GET /users/{id}", app.showUserHandler)
-	router.HandleFunc("GET /api/authenticate", app.getUserForToken)
-	router.HandleFunc("POST /api/users", app.createUserHandler)
-	router.HandleFunc("POST /api/login", app.authenticateUser)
-	router.HandleFunc("POST /api/refresh_session", app.refreshSession)
-	router.HandleFunc("POST /api/posts", app.ValidateJwt(app.createPostHandler))
+	router.Handle("GET /",
+		app.ValidateJwt(app.HomeHandler))
+	router.HandleFunc("GET /apo/users/{id}",
+		app.showUserHandler)
+	router.HandleFunc("POST /api/users",
+		app.createUserHandler)
+	router.HandleFunc("GET /api/users/{username}/posts",
+		app.getUserPostsHandler)
+	router.HandleFunc("GET /api/authenticate",
+		app.getUserForToken)
+	router.HandleFunc("POST /api/login",
+		app.authenticateUser)
+	router.HandleFunc("POST /api/refresh_session",
+		app.refreshSession)
+	router.HandleFunc("POST /api/posts",
+		app.ValidateJwt(app.createPostHandler))
 
 	return app.RecoverPanic(app.rateLimit(app.SecureHeaders(app.LogRequest(router))))
 }
