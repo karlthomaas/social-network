2945ea69532d55b9ca32763b07725c9f1416afc0
Kristo Veber <kristoveber7@gmail.com>
2024-04-04 14:27:49 +0300

    Refactor PostModel to include title and updated_at fields

--- internal/data/posts.go
+++ internal/data/posts.go
@@ -23,27 +23,30 @@ type Post struct {
 
 func (m *PostModel) Insert(post *Post) error {
 	query := `
-		INSERT INTO posts (id, user_id, content, image, privacy)
-		VALUES (?,?,?,?,?)`
+		INSERT INTO posts (id, title, user_id, content, image, privacy, updated_at)
+		VALUES (?,?,?,?,?,?,?)`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	args := []interface{}{
 		post.ID,
+		post.Title,
 		post.UserID,
 		post.Content,
 		post.Image,
 		post.Privacy,
+		post.UpdatedAt,
 	}
 
-	return m.DB.QueryRowContext(ctx, query, args...).Scan(&post.ID, &post.UserID, &post.CreatedAt)
+	_, err := m.DB.ExecContext(ctx, query, args...)
+	return err
 }
 
 func (m *PostModel) Update(post *Post) error {
 	query := `
 		UPDATE posts
-		SET title = ? content = ?, image = ?
+		SET title = ?, content = ?, image = ?
 		WHERE user_id = ? AND id = ?
 	`
 
@@ -60,4 +63,48 @@ func (m *PostModel) Update(post *Post) error {
 
 	_, err := m.DB.ExecContext(ctx, query, args...)
 	return err
+}
+
+func (m *PostModel) GetAllForUser(userID string) ([]*Post, error) {
+	query := `
+		SELECT id, title, user_id, content, image, privacy, created_at, updated_at
+		FROM posts
+		WHERE user_id = ?`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	posts := []*Post{}
+
+	for rows.Next() {
+		var post Post
+
+		err := rows.Scan(
+			&post.ID,
+			&post.Title,
+			&post.UserID,
+			&post.Content,
+			&post.Image,
+			&post.Privacy,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, &post)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return posts, nil
 }
