f26a744bdfdf80dedf57fafffb962d364cbb6016
Kristo Veber <kristoveber7@gmail.com>
2024-04-21 18:04:39 +0300

    Add deleteReplyHandler, getAllPostRepliesHandler, and updateReplyHandler functions to handle reply deletion, retrieval, and update in replies.go

--- cmd/api/replies.go
+++ cmd/api/replies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"social-network/internal/data"
 	"time"
@@ -45,6 +46,123 @@ func (app *application) createReplyHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"reply": reply}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
+
+
+func (app *application) deleteReplyHandler(w http.ResponseWriter, r *http.Request) {
+
+	currentUser := app.contextGetUser(r)
+
+	replyID, err := app.readParam(r, "replyID")
+	if err != nil {
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	reply, err := app.models.Replies.Get(replyID)
+	if err != nil {
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	if reply.UserID != currentUser.ID {
+		app.unAuthorizedResponse(w,r)
+		return
+	}
+
+	err = app.models.Replies.Delete(replyID)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+
+	err = app.writeJSON(w,http.StatusOK,envelope{"reply":"successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+}
+
+
+func (app *application) getAllPostRepliesHandler(w http.ResponseWriter, r *http.Request) {
+	postID, err := app.readParam(r, "id")
+	if err != nil {
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	replies, err := app.models.Replies.GetAll(postID)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w,r)
+		default:
+			app.serverErrorResponse(w,r,err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"replies":replies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+}
+
+func (app *application) updateReplyHandler(w http.ResponseWriter, r *http.Request) {
+	currentUser := app.contextGetUser(r)
+
+	var input struct {
+		Content *string `json:"content"`
+		Image   *[]byte `json:"image"`
+	}
+
+	replyID, err := app.readParam(r, "replyID")
+	if err != nil {
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	reply, err := app.models.Replies.Get(replyID)
+	if err != nil {
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	if reply.UserID != currentUser.ID {
+		app.unAuthorizedResponse(w,r)
+		return
+	}
+
+	if input.Content != nil {
+		reply.Content = *input.Content
+	}
+
+	if input.Image != nil {
+		reply.Image = *input.Image
+	}
+
+	if input.Image != nil {
+		reply.Image = *input.Image
+	}
+
+	reply.UpdatedAt = time.Now().Truncate(time.Second)
+
+	err = app.models.Replies.Update(reply)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"reply": reply}, nil)
