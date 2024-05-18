f7a2ebc84a28e3a9598d90830983ccc3cffb907e
Kristo Veber <kristoveber7@gmail.com>
2024-05-18 20:39:23 +0300

    add routes for messages

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -143,6 +143,10 @@ func (app *application) routes() http.Handler {
 	router.HandleFunc("POST /api/images/{options}/{id}",
 		app.ValidateJwt(app.createImageHandler))
 
+	router.HandleFunc("GET /api/messages/users/{userID}",
+		app.ValidateJwt(app.getPrivateMessagesHandler))
+	router.HandleFunc("GET /api/messages/groups/{groupID}",
+		app.ValidateJwt(app.getGroupMessagesHandler))
 
 	router.HandleFunc("/ws", app.ValidateJwt(app.wsHandler))
 
